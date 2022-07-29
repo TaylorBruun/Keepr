dfaf7f42fddb876cfca6f877ff326e91af183400
TaylorBruun <taylorbruun@gmail.com>
2022-07-29 14:59:36 -0600

    tables exist, db strings

--- Keepr/Models/Keep.cs
+++ Keepr/Models/Keep.cs
@@ -8,6 +8,7 @@ namespace Keepr.Models
         public string Img { get; set; }
         public int Views { get; set; }
         public int Kept { get; set; }
+        public int Shares { get; set; } //Stretch Goal
         public string CreatorId { get; set; }
         public Profile Creator { get; set; }
     }
--- Keepr/dbSetup.sql
+++ Keepr/dbSetup.sql
@@ -1,8 +1,45 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8 COMMENT '';
+CREATE TABLE
+    IF NOT EXISTS accounts(
+        id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        name varchar(255) COMMENT 'User Name',
+        email varchar(255) COMMENT 'User Email',
+        picture varchar(255) COMMENT 'User Picture'
+    ) default charset utf8 COMMENT '';
+
+CREATE TABLE
+    IF NOT EXISTS keeps(
+        id INT(255) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        img VARCHAR(255) NOT NULL,
+        views INT NOT NULL,
+        kept INT NOT NULL,
+        shares INT NOT NULL,
+        creatorId VARCHAR(255),
+        FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) default charset utf8 COMMENT '';
+
+CREATE TABLE
+    IF NOT EXISTS vaults(
+        id INT(255) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        isPrivate TINYINT NOT NULL,
+        creatorId VARCHAR(255),
+        FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) default charset utf8 COMMENT '';
+
+CREATE TABLE
+    IF NOT EXISTS vaultKeeps(
+        id INT(255) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        vaultId INT NOT NULL,
+        keepId INT NOT NULL,
+        creatorId VARCHAR(255),
+        FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE,
+        FOREIGN KEY (vaultId) REFERENCES vaults(id),
+        FOREIGN KEY (keepId) REFERENCES keeps(id)
+    ) default charset utf8 COMMENT '';
+
+) 
