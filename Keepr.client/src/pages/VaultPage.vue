c2196e73003ea45af87ca118194a6cbf398e025d
TaylorBruun <taylorbruun@gmail.com>
2022-07-31 17:04:28 -0600

    home/profile pages have vault/keep data

---
+++ Keepr.client/src/pages/VaultPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="component">
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    setup(){
+        return {}
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+  .masonry-frame {
+    columns: 4;
+  
+    div {
+      break-inside: avoid;
+    }
+  }
+@media (max-width: 768px) {
+  .masonry-frame {
+    columns: 2;
+
+    div {
+      break-inside: avoid;
+    }
+  }
+}
+</style>
