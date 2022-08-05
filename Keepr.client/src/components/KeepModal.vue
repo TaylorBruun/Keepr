6af60c4e8a1130838a1d0c0f6cbaca686affa4f0
TaylorBruun <taylorbruun@gmail.com>
2022-08-05 10:01:23 -0600

    tinker with keep modal styling

--- Keepr.client/src/components/KeepModal.vue
+++ Keepr.client/src/components/KeepModal.vue
@@ -1,66 +1,88 @@
 <template>
     <div class="modal fade" id="keep-modal" tabindex="-1" aria-labelledby="" aria-hidden="true">
-        <div class="modal-dialog modal-xl modal-fullscreen-lg-down modal-dialog-centered">
+        <div class="modal-dialog modal-xl  modal-dialog-centered">
             <div class="modal-content ">
-                <div class="row  border-0 modal-header">
 
-                    <div @click.stop="goToProfile" class=" col-7 d-flex m-1 p-1 align-items-center">
-                        <img  class="selectable creator-img" :src="keep?.creator?.picture" alt="">
-                        <h6 class="selectable m-2 text-break">
-                            {{ keep?.creator?.name }}
-                        </h6>
-                    </div>
-                    <div class="col-2">
-                        <button type="button" title="Close" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                </div>
                 <div class="modal-body">
                     <div class="row">
                         <div class="col-6">
 
                             <img class="img-fluid" :src="keep.img" alt="">
-                            <div class="row d-flex align-items-center">
-                                <!-- add to vault -->
-                                <div class="col-5 me-5 m-2">
-                                    <div class="btn-group dropup">
-                                        <button v-if="account.id" type="button" class="btn drop-btn  dropdown-toggle"
-                                            data-bs-toggle="dropdown" aria-expanded="false">
-                                            Add to Vault
-                                        </button>
-                                        <ul class="dropdown-menu">
-                                            <AddToVault v-for="v in userVaults" :key="v.id" :vault="v" />
-                                        </ul>
-                                    </div>
-                                </div>
-                                <!-- delete if creator -->
-                                <div v-if="keep.creatorId == account.id" @click.stop="deleteKeep"
-                                    class="col-2 selectable p-2 ms-3">
-                                    <i title="Delete" class="delete-icon mdi mdi-delete">Delete</i>
-                                </div>
-                            </div>
+
                         </div>
-                        <div class="col-6">
-                            <div class="row p-0 border-0 modal-header">
-                                <div class="col-4">
-                                    <h6 class="activity-icon"><i class="mdi mdi-key-star"></i> {{ keep.kept }}</h6>
+
+
+
+
+                        <div class="col-6 fill flex-grow-1">
+                            <div class="row">
+                                <div class="col d-flex flex-column ">
+
+
+                                    <div class="row pb-3 align-items-center justify-content-between">
+                                        <div @click.stop="goToProfile" class=" col-7 d-flex m-1 p-1 align-items-center">
+                                            <img class="selectable creator-img" :src="keep?.creator?.picture" alt="">
+                                            <h6 class="selectable m-2 text-break">
+                                                {{ keep?.creator?.name }}
+                                            </h6>
+                                        </div>
+                                        <div class="col-2">
+                                            <button type="button" title="Close" class="btn-close"
+                                                data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="row p-0 pb-1 border-0 modal-header">
+                                        <div class="col-4">
+                                            <h6 class="activity-icon"><i class="mdi mdi-key-star"></i> {{ keep.kept }}
+                                            </h6>
+                                        </div>
+                                        <!-- NOTE v-if false means it will never show. Enable if shares gets implemented -->
+                                        <div v-if="false" class="col-4">
+                                            <h6 class="activity-icon"><i class="mdi mdi-share-variant-outline"></i> {{
+                                                    keep.shares
+                                            }}</h6>
+                                        </div>
+                                        <div class="col-4">
+                                            <h6 class="activity-icon"><i class="mdi mdi-eye"></i> {{ keep.views }}</h6>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <h2 class="border-bottom display-4">{{ keep.name }}</h2>
+                                    </div>
+                                    <div class="">
+                                        <h6 class="text-muted b0"><em>
+                                                {{ keep.description }}
+                                            </em></h6>
+                                    </div>
+
+
+                                    <div class="row position-absolute bottom-0 d-flex align-items-center mt-5 pt-5 pb-3">
+                                        <!-- add to vault -->
+                                        <div class=" col-5 me-5 m-2">
+                                            <div class="btn-group dropup">
+                                                <button v-if="account.id" type="button"
+                                                    class="btn drop-btn  dropdown-toggle" data-bs-toggle="dropdown"
+                                                    aria-expanded="false">
+                                                    Add to Vault
+                                                </button>
+                                                <ul class="dropdown-menu">
+                                                    <AddToVault v-for="v in userVaults" :key="v.id" :vault="v" />
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <!-- delete if creator -->
+                                        <div v-if="keep.creatorId == account.id" @click.stop="deleteKeep"
+                                            class="col-2 selectable p-2 ms-3">
+                                            <i title="Delete" class="delete-icon mdi mdi-delete"></i>
+                                        </div>
+                                    </div>
+
+
+
+
                                 </div>
-                                <!-- NOTE v-if false means it will never show. Enable if shares is implemented -->
-                                <div v-if="false" class="col-4">
-                                    <h6 class="activity-icon"><i class="mdi mdi-share-variant-outline"></i> {{
-                                            keep.shares
-                                    }}</h6>
-                                </div>
-                                <div class="col-4">
-                                    <h6 class="activity-icon"><i class="mdi mdi-eye"></i> {{ keep.views }}</h6>
-                                </div>
-                            </div>
-                            <div>
-                                <h2 class="border-bottom display-4">{{ keep.name }}</h2>
-                            </div>
-                            <div>
-                                <h6 class="text-muted"><em>
-                                        {{ keep.description }}
-                                    </em></h6>
                             </div>
                         </div>
                     </div>
@@ -111,6 +133,13 @@ export default {
     color: $keepr-primary;
     font-size: 1.3rem;
 }
+
+
+.b0{
+    // background-color: blue;
+    min-height: 15vh;
+}
+
 
 .creator-img {
     width: 30px;
